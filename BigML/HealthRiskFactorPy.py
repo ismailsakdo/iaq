b326d3bd86004814d2b4cdca90a9bce606462880
Ismail SA Kajidataonline <57325497+ismailsakdo@users.noreply.github.com>
2022-09-05 10:07:15 +0800

    Create HealthRiskFactorPy.py

---
+++ BigML/HealthRiskFactorPy.py
@@ -0,0 +1,32 @@
+# Requires BigML Python bindings
+#
+# Install via: pip install bigml
+#
+# or clone it:
+#   git clone https://github.com/bigmlcom/python.git
+from bigml.deepnet import Deepnet
+from bigml.api import BigML
+# Downloads and generates a local version of the DEEPNET,
+# if it hasn't been downloaded previously.
+deepnet = Deepnet('deepnet/6311e077aba2df5335001067',
+                  api=BigML("myibrick",
+                            "ef928b5c251cff2ec6fdc612e0d220299edce8e0",
+                            domain="bigml.io"))
+# To make predictions fill the desired input_data in next line.
+input_data = {
+    "co2.cat": "0",
+    "airflow.cat": "0",
+    "tvoc.cat": "0",
+    "metsec.cat": "1",
+    "hcho.cat": "0",
+    "pm2.5cat": "0",
+    "totalrisk": 1
+}
+deepnet.predict(input_data, full=True)
+#
+# input_data: dict for the input values
+# (e.g. {"petal length": 1, "sepal length": 3})
+# full: if set to True, the output will be a dictionary that includes all the
+# available information about the prediction. The attributes vary depending
+# on the ensemble type. Please check:
+# https://bigml.readthedocs.io/en/latest/#local-deepnet-predictions
